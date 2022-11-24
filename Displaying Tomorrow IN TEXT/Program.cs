85dd2c77a38e1b9d7324b93a0daa0d7f594f511b
Okafornta <hillaryonia@gmail.com>
2022-11-24 10:27:51 +0100

    Add project files.

---
+++ Displaying Tomorrow IN TEXT/Program.cs
@@ -0,0 +1,15 @@
+﻿//TASK: I will write a program that displays tomorrow’s date.
+
+// Today's date 
+DateTime today = DateTime.Today;
+
+// Tomorrow date
+DateTime tomorrow = today.AddDays(1);
+
+// Output 
+Console.WriteLine("Today is " + today.ToLongDateString() + ".\n");
+
+Console.WriteLine("I want to be good with C# before " + tomorrow.ToLongDateString() + ".");
+
+// Waiting for Enter
+Console.ReadLine();
